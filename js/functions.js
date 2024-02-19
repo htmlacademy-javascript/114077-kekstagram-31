32c45a02d5d90e6b938a3f55285b63cd308860a3
Ivan Zagainov <fohunoff@gmail.com>
2024-02-19 19:11:43 +0300

    добавляет функцию проверки фразы на палиндром

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,32 @@
 const checkStrLimit = (str = '', limit = 0) => str.toString().length <= limit;
 
+const isPalindrome = (input = '') => {
+  const phrase = input ?? '';
+
+  if (typeof phrase !== 'string' || phrase.length === 0) {
+    return new Error('Палиндром можно получить только для строки');
+  }
+
+  const normalizePhrase = phrase.replaceAll(' ', '').toLowerCase();
+  let palindrome = '';
+
+  for (let i = normalizePhrase.length - 1; i >= 0; i--) {
+    palindrome += normalizePhrase.at(i);
+  }
+
+  return palindrome === normalizePhrase;
+};
+
+let notUse;
+
 checkStrLimit('проверяемая строка', 20);
 checkStrLimit('проверяемая строка', 18);
 checkStrLimit('проверяемая строка', 10);
+
+isPalindrome();
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
+isPalindrome('Лёша на полке клопа нашёл ');
+isPalindrome(null);
+isPalindrome(notUse);
